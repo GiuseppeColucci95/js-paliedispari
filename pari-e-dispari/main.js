ab1b220affb96b795dc8a9c94847668724d99c13
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-14 15:16:14 +0100

    add pseudocode of pari e dispari

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,3 +1,15 @@
-/* 
+/*
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
+
+//ask to the user if even or odd and save it in a variable
+
+//ask to the user a number between 1 and 5 and save it in a variable
+
+//generate a random number between 1 and 5 using a function and save it in a variable
+
+//sum the numbers
+
+//establish if sum is even or odd using a function
+
+//print the result
